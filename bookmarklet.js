bacc9a7e15d7eb4020020f79f1d3213253cda741
Sérgio Lopes <sergio.lopes@caelum.com.br>
2019-11-25 16:23:15 -0300

    bookmarklet novo, com hls

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,7 @@
-javascript:window.open('https://caelum.github.io/alura-transcritor/transcritor.html?video=' + encodeURIComponent(document.querySelector('video').src) + '&admin=' + encodeURIComponent(document.querySelector('.settings-box-item-edit a').href) + '&task=' + encodeURIComponent(location.href) + '&posicao=' + document.querySelector('.task-body-header-title small').innerText + '&aula=' + document.querySelector('.task-menu-section-title-number strong').innerText, 'transcritor');
+javascript:(function(){
+    fetch(location.href + '/video', {useCredentials: true}).then(res => res.json())
+    .then(data => {
+        var video = data[0].link.replace(/\/hls/, '').replace(/\/index.m3u8/,'');
+        window.open('https://caelum.github.io/alura-transcritor/transcritor.html?video=' + encodeURIComponent(video) + '&admin=' + encodeURIComponent(document.querySelector('.settings-box-item-edit a').href) + '&task=' + encodeURIComponent(location.href) + '&posicao=' + document.querySelector('.task-body-header-title small').innerText + '&aula=' + document.querySelector('.task-menu-section-title-number strong').innerText, 'transcritor');
+    });
+})()
