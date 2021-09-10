522c69d933a3c34926a5e95a2bbe3700446f5705
Sérgio Lopes <sergio.lopes@caelum.com.br>
2021-09-10 17:23:39 -0300

    suporte ao youtube

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,11 @@
 javascript:(function(){
-    fetch(location.href + '/video', {useCredentials: true}).then(res => res.json())
-    .then(data => {
-        var video = data[0].link.replace(/\/hls/, '').replace(/\/index.m3u8/,'');
-        window.open('https://caelum.github.io/alura-transcritor/transcritor.html?video=' + encodeURIComponent(video) + '&admin=' + encodeURIComponent(document.querySelector('.settings-box-item-edit a').href) + '&task=' + encodeURIComponent(location.href) + '&posicao=' + document.querySelector('.task-body-header-title small').innerText + '&aula=' + document.querySelector('.task-menu-section-title-number strong').innerText, 'transcritor');
-    });
+    if (location.host == 'www.youtube.com') {
+        window.open('https://caelum.github.io/alura-transcritor/transcritor.html?youtube=' + new URLSearchParams(location.search).get('v'), 'transcritor');
+    } else {
+        fetch(location.href + '/video', {useCredentials: true}).then(res => res.json())
+        .then(data => {
+            var video = data[0].link.replace(/\/hls/, '').replace(/\/index.m3u8/,'');
+            window.open('https://caelum.github.io/alura-transcritor/transcritor.html?video=' + encodeURIComponent(video) + '&admin=' + encodeURIComponent(document.querySelector('.settings-box-item-edit a').href) + '&task=' + encodeURIComponent(location.href) + '&posicao=' + document.querySelector('.task-body-header-title small').innerText + '&aula=' + document.querySelector('.task-menu-section-title-number strong').innerText, 'transcritor');
+        });    
+    }
 })()
