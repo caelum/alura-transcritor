c7b7247984e4fdf0a59503ccbd2955a167a07283
Carlos Eduardo Müller de Carvalho <kdu428@gmail.com>
2023-01-26 12:47:18 -0300

    fix(booklet): ajustando a modelagem da resposta do servidor

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,7 +4,7 @@ javascript:(function(){
     } else {
         fetch(location.href + '/video', {useCredentials: true}).then(res => res.json())
         .then(data => {
-            var video = data[0].link.replace(/\/hls/, '').replace(/\/index.m3u8/,'');
+             var video = data[0].mp4.replace(/\/hls/, '').replace(/\/index.m3u8/,'');
             window.open('https://caelum.github.io/alura-transcritor/transcritor.html?video=' + encodeURIComponent(video) + '&admin=' + encodeURIComponent(document.querySelector('.settings-box-item-edit a').href) + '&task=' + encodeURIComponent(location.href) + '&posicao=' + document.querySelector('.task-body-header-title small').innerText + '&aula=' + document.querySelector('.task-menu-section-title-number strong').innerText, 'transcritor');
         });    
     }
